35ac51f52bba5281ed32046a326b6735f90e179c
Kaveesha Weerasiri <kweera2@uic.edu>
2020-04-24 21:51:58 +0000

    commented out the service worker, but I think it works. Took out the image that it caches

--- js/app.js
+++ js/app.js
@@ -3,28 +3,28 @@ const virus = [
   { name: "VirusPic", image: "images/virus.jpg" },
 ]
 
-const showVirus = () => {
-  let output = ""
-  virus.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  )
-  container.innerHTML = output
-}
+// const showVirus = () => {
+//   let output = ""
+//   virus.forEach(
+//     ({ name, image }) =>
+//       (output += `
+//               <div class="card">
+//                 <img class="card--avatar" src=${image} />
+//                 <h1 class="card--title">${name}</h1>
+//                 <a class="card--link" href="#">Taste</a>
+//               </div>
+//               `)
+//   )
+//   container.innerHTML = output
+// }
 
-document.addEventListener("DOMContentLoaded", showVirus)
+// document.addEventListener("DOMContentLoaded", showVirus)
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function() {
+//     navigator.serviceWorker
+//       .register("/serviceWorker.js")
+//       .then(res => console.log("service worker registered"))
+//       .catch(err => console.log("service worker not registered", err))
+//   })
+// }
