cfb87aef32c3df16441a25dcc9c7f60ec7e5b32d
Kaveesha Weerasiri <kweera2@uic.edu>
2020-04-29 15:53:09 +0000

    almost got the service worker working

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,25 +1,96 @@
-const bigProject = "big-project-v1"
+const CACHE_NAME = 'big-project-v5';
 //stores all of the files so that it can be accessed offline
-const assets = [
-  "/",
-  "/index.html",
-  "/js/app.js",
-  "/images/virus.jpg",
-]
+let urlsToCache = [
+  './index.html',
+  'https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css',
+  'https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  './icons/android-chrome-192x192.png',
+  './icons/android-chrome-512x512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-16x16.png',
+  './icons/favicon-32x32.png',
+];
+
+// //installs the service worker
+// //install, put these stuff into cache
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
 //installs the service worker
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(bigProject).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+//install, put these stuff into cache
+// self.addEventListener('install', (event) => {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache.map((urlToPrefetch) => {
+//                   return new Request(urlsToCache, { mode: 'no-cors' });
+//                 })).then(() => {
+//                   console.log('All resources have been fetched and cached.');
+//                 });
+//       })
+//   );
+// });
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+//deletes all unwated cache
+self.addEventListener('activate', (event) => {
+  console.log("activating");
+  let cacheWhitelist = ['big-project-v5'];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
-  )
-})
+  );
+});
+
+//look at cache first, if not there then fetch it
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
